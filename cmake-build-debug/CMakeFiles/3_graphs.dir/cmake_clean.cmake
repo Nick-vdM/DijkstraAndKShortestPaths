be8c01b6d7a6a5929f15e13fb4b81e19285e1c42
Nick-vdM <nick.jvdmerwe@gmail.com>
2020-04-26 19:32:50 +1000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/3_graphs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3_graphs"
+  "3_graphs.pdb"
+  "CMakeFiles/3_graphs.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_graphs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/3_graphs.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 3_graphs.
+# This may be replaced when dependencies are built.
